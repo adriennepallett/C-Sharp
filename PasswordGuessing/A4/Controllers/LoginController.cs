1a3b57c5d5d87355637ce8e3280019de796eabfc
AdrienneP-hoenix86 <adriennepallett@icloud.com>
2024-04-25 08:31:41 -0400

    Password Guessing
    This program generates a web app that can be used to login, or as I created it, a game of guessing logins.
    
    For pregenerated user logins, see README

---
+++ PasswordGuessing/A4/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using A4.Models;
+using A4.Services;
+
+namespace A4.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel user)
+        {
+            Users users = new Users();
+            if (users.isValid(user))
+            {
+                return View("GreenLight", user);
+            }
+            return View("RedLight", user);
+        }
+    }
+}
+
---
+++ PasswordGuessing/A4/Services/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using A4.Models;
+
+namespace A4.Services
+{
+	public class Users
+	{
+		List<UserModel> userList = new List<UserModel>();
+
+		public Users()
+		{
+			userList.Add(new UserModel { ID = 1, Username = "Adrienne", Password = "Password" });
+            userList.Add(new UserModel { ID = 1, Username = "Mike", Password = "Blue123" });
+            userList.Add(new UserModel { ID = 1, Username = "John", Password = "Green456" });
+            userList.Add(new UserModel { ID = 1, Username = "Rachel", Password = "NewYork" });
+            userList.Add(new UserModel { ID = 1, Username = "Silas", Password = "ASD" });
+        }
+
+		public bool isValid(UserModel user)
+		{
+			return userList.Any(x => x.Username == user.Username && x.Password == user.Password);
+		}
+	}
+}
+
